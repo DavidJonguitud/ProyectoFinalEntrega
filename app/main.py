a6136110224f04121546e579559d8efc980d4d21
Angel Peña <angel_pena1@epam.com>
2026-08-19 23:14:45 -0600

    feat: add storage strategy logging and new PDF file for project 7

--- app/main.py
+++ app/main.py
@@ -17,11 +17,15 @@ from app.core.database import create_database_if_not_exists
 from app.routers.documents import document_router
 from app.routers.project import project_router
 from app.routers.user import user_router
+from app.core.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_database_if_not_exists()
+    print("\n" + "="*50)
+    print(f"ESTRATEGIA DE ALMACENAMIENTO ACTIVA: {settings.STORAGE_STRATEGY}")
+    print("="*50 + "\n")
 
     alembic_cfg = Config("alembic.ini")
     command.upgrade(alembic_cfg, "head")
